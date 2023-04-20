34b2ce598dd894c558d65cf17b3aa18d161dc470
Prachi tote <prachi.tote77@gmail.com>
2023-04-20 16:34:06 +0530

    Fibonacci Series Program

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("Welcome to Fibonacci Program");
+            Fibonacci fibonacci = new Fibonacci();
+            fibonacci.Fibonac();
+        }
+    }
+}
