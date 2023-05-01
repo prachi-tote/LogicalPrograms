07689514815db0a70d0c5910e78d64001b28c9b3
prachi-tote <127733520+prachi-tote@users.noreply.github.com>
2023-05-01 10:31:10 +0530

    Update PrimeNumber.cs

--- LogicalPrograms/PrimeNumber.cs
+++ LogicalPrograms/PrimeNumber.cs
@@ -14,9 +14,7 @@ namespace LogicalPrograms
             int n, i, m = 0, flag = 0;
 
             n = int.Parse(Console.ReadLine());
-            m = n / 2;
-
-            m = n / 2;
+            m = n / 2 ;
             for (i = 2; i <= m; i++)
             {
                 if (n % i == 0)
